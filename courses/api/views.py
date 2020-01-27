d97a61ae251e2644545e079058143149d838f122
Sergey N <snorov@gmail.com>
2020-01-27 14:38:32 +0300

    contents view

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,12 +1,15 @@
 from rest_framework import generics
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from ..models import Course, Subject
-from .serializers import SubjectSerializer, CourseSerializer
+from .serializers import SubjectSerializer, CourseSerializer, CourseWithContentsSerializer
+from .permissions import IsEnrolled
+
 
 
 class SubjectListView(generics.ListAPIView):
@@ -30,3 +33,18 @@ class CourseEnrollView(APIView):
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    @action(detail=True,methods=['post'],
+                      authentication_classes=[BasicAuthentication],
+                      permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+
+    @action(detail=True,methods=['get'],
+                            serializer_class=CourseWithContentsSerializer,
+                            authentication_classes=[BasicAuthentication],
+                            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
